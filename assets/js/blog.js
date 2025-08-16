c570e975d302931fcd01e20cad078059c68dcd59
53R4PH1N3 <baho1235.archer.helix.baho1235@gmail.com>
2025-08-16 15:44:59 +0800

    update blog page

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,4 +1,4 @@
-(async function() {
+(async function() { 
     await axios.get("https://dev.to/api/articles?username=beroller")
     .then((response) => {
         const blogData = response.data;
@@ -15,17 +15,31 @@
         }
 
         blogData.forEach(el => {
+            var tagsStr = '';
             var list = document.createElement('div');
-            list.classList.add("col-6")
+            list.classList.add("col-4")
+            
+            for (let index = 0; index < el.tag_list.length; index++) {
+                const tag = el.tag_list[index];
+                tagsStr += `<span class="me-1 badge badge-secondary">#${tag}</span>`
+            }
+
             list.setHTMLUnsafe(`
-            <div class="card m-2" data-id="${el.id}">
-                ${(el.cover_image != null)? `<img class="card-img-top" src="${el.cover_image}" alt="Cover Image"/>` : ''}
-                <div class="card-body">
-                    <h4 class="card-title pb-3"><a target="_blank" href="${el.url}">${el.title}</a></h4>
-                    <h6 class="card-subtitle mb-2 text-muted">${el.description}</h6>
-                    <p class="pt-3 card-text text-end"><small class="text-muted">${el.readable_publish_date} - ${el.user.name}</small></p>
-                </div>
-            </div`)
+            <a target="_blank" href="${el.url}">
+                <div class="card m-2" data-id="${el.id}">
+                    ${(el.cover_image != null)? `<img class="card-img-top" src="${el.cover_image}" alt="Cover Image"/>` : ''}
+                    <div class="card-body">
+                        <h4 class="card-title pb-3">${el.title}</h4>
+                        <h6 class="card-subtitle mb-2 text-muted">${el.description}</h6>
+                        <div id="hastags">
+                            <p>
+                                ${tagsStr}
+                            </p>
+                        </div>
+                        <p class="pt-3 card-text text-end"><small class="text-muted">${el.readable_publish_date} - ${el.user.name}</small></p>
+                    </div>
+                </div
+            </a>`)
             
             const spinner = document.querySelector(`.spinners`);
             if(spinner) spinner.remove()
